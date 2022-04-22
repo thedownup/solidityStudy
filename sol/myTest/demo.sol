3f6b7b7bfd9b3cb539d43df5690cb6d46f65a0ff
aywm <jinteng.zhou@1233s2b.com>
2022-04-23 00:31:45 +0800

    add

--- sol/myTest/demo.sol
+++ sol/myTest/demo.sol
@@ -1,18 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.8.9;
 
-interface Idemo {
-    function read() external view returns (uint256);
-
-    function totalSupply() external view returns (uint256);
-}
-
-contract demo {
-    string public name = "test";
-
-    int num = 666;
-
-    function getName() public view returns (int) {
-        return num;
+contract Test {
+    function test(uint a, uint b) public pure returns (uint) {
+        return a + b;
     }
 }
