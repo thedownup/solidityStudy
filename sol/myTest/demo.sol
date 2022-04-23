6b420af61f184030635a6aa83d17ceffa225175d
aywm <jinteng.zhou@1233s2b.com>
2022-04-23 16:43:40 +0800

    add lib

--- sol/myTest/demo.sol
+++ sol/myTest/demo.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 contract Test {
     function test(uint a, uint b) public pure returns (uint) {
         return a + b;
@@ -11,6 +13,13 @@ contract Test {
         int age;
     }
 
+    Person[] public people;
+
     Person public p = Person("1", 1);
 
+    int age = 0;
+
+    function getPerson1123() public view returns (int) {
+        // return getAge();
+    }
 }
